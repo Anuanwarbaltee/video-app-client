f21e6d0b244e5a0bded6fc5f84a34d3d6a7d736f
Anuanwarbaltee <anwardines786@gmail.com>
2026-01-08 23:45:16 +0500

    update dev url

--- src/Services/Appsetting.jsx
+++ src/Services/Appsetting.jsx
@@ -1,9 +1,9 @@
 const AppSetting = {
     // local
-    baseUrl: "http://localhost:8000/api/v1/"
+    // baseUrl: "http://localhost:8000/api/v1/"
 
     // dev
-    // baseUrl: "https://video-streaming-app-production-8f3e.up.railway.app/api/v1/"
+    baseUrl: "https://video-streaming-app-production-8f3e.up.railway.app/api/v1/"
 }
 
 
