0c411d11950f4fa16b22beb66faaa43477c9c7b8
Anuanwarbaltee <anwardines786@gmail.com>
2026-01-14 19:33:42 +0500

    vercel analytic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import './App.css'
 import { CustomStyle } from './Shell/CustomStyles';
 import Router from './Shell/Router'
 import GlobalStyles from '@mui/material/GlobalStyles';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <>
       <GlobalStyles styles={CustomStyle} />
       <Router />
-
+      <Analytics />
     </>
   )
 }
