d3961bfea0f5ea7358596d76f8d367309e88e622
Anuanwarbaltee <anwardines786@gmail.com>
2026-02-03 13:28:58 +0500

    add replit api url

--- src/Services/Appsetting.jsx
+++ src/Services/Appsetting.jsx
@@ -1,9 +1,11 @@
 const AppSetting = {
     // local
-    // baseUrl: "http://localhost:8000/api/v1/"
+    // baseUrl: "http://localhost:5000/api/v1/"
 
-    // dev
-    baseUrl: "https://video-streaming-app-production-8f3e.up.railway.app/api/v1/"
+    //railway  dev
+    // baseUrl: "https://video-streaming-app-production-8f3e.up.railway.app/api/v1/"
+    //replit url
+    baseUrl: "https://video-streaming-app--Anu786786.replit.app/api/v1/"
 }
 
 
